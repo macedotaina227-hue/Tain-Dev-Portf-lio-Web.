add970e3d7199de29a93c5d18f5feef9f03d27ee
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-16 15:22:44 -0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Tainá Macedo Pessoa</h1>
+      <p>Acadêmica em Tecnologia de Sistemas para Internet</p>
+    </header>
+  );
+}
+
+export default Header;
