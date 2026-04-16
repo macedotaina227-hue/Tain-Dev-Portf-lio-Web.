2ed101e5d099f72146b54683fbe7a9d225ad5e00
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-16 15:29:26 -0300

    Create App.js

---
+++ src/data/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
