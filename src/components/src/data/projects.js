59b03c5aea76135d701dd7318e35cee30dbd5701
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-16 15:23:57 -0300

    Create projects.js

---
+++ src/components/src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    id: 1,
+    name: "Anti Ansiedade",
+    description: "Aplicação para ajudar no controle da ansiedade com técnicas de respiração e organização emocional.",
+    link: "#"
+  },
+  {
+    id: 2,
+    name: "Agenda Diária",
+    description: "Sistema simples para organizar tarefas do dia a dia e melhorar a produtividade.",
+    link: "#"
+  }
+];
+
+export default projects;
